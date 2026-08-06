256c8f24e57df50d458b16ccfa5e15335801d67b
gaus-dev <gaus29.dev@gmail.com>
2026-08-06 13:59:21 +0530

    Adding interactivity using astro islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://low-carbon-site.netlify.app/"
+  site: "https://low-carbon-site.netlify.app/",
+  integrations: [preact()]
 });
